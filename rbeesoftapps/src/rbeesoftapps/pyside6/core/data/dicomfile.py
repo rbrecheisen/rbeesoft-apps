1ff6d9acd54551096083d4f5d78f50dde583cc24
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-01-11 17:25:34 +0100

    Saving work

--- rbeesoftapps/src/rbeesoftapps/pyside6/core/data/dicomfile.py
+++ rbeesoftapps/src/rbeesoftapps/pyside6/core/data/dicomfile.py
@@ -8,6 +8,15 @@ class DicomFile(File):
     def __init__(self, path: str) -> None:
         super(DicomFile, self).__init__(path)
         self._data = None
+        self._series_instance_uid = None
+        self._instance_number = None
+        self._patient_id = None
+        self._rows = None
+        self._columns = None
+        self._slice_thickness = None
+        self._modality = None
+        self._series_description = None
+        self._manufacturer = None
 
     def load(self) -> bool:
         self._data = None
@@ -15,9 +24,54 @@ class DicomFile(File):
             return False
         try:
             self._data = pydicom.dcmread(self.path())
+            if 'SeriesInstanceUID' in self._data:
+                self._series_instance_uid = self._data.SeriesInstanceUID
+            if 'InstanceNumber' in self._data:
+                self._instance_number = self._data.InstanceNumber
+            if 'PatientID' in self._data:
+                self._patient_id = self._data.PatientID
+            if 'Rows' in self._data:
+                self._rows = self._data.Rows
+            if 'Columns' in self._data:
+                self._columns = self._data.Columns
+            if 'SliceThickness' in self._data:
+                self._slice_thickness = self._data.SliceThickness
+            if 'Modality' in self._data:
+                self._modality = self._data.Modality
+            if 'SeriesDescription' in self._data:
+                self._series_description = self._data.SeriesDescription
+            if 'Manufacturer' in self._data:
+                self._manufacturer = self._data.Manufacturer
             return True
         except pydicom.errors.InvalidDicomError:
             return False
         
     def data(self) -> pydicom.FileDataset:
         return self._data
+    
+    def series_instance_uid(self) -> str:
+        return self._series_instance_uid
+    
+    def instance_number(self) -> int|None:
+        return int(self._instance_number) if self._instance_number else None
+    
+    def patient_id(self) -> str:
+        return self._patient_id
+    
+    def rows(self) -> int|None:
+        return int(self._rows) if self._rows else None
+    
+    def columns(self) -> int|None:
+        return int(self._columns) if self._columns else None
+    
+    def slice_thickness(self) -> float|None:
+        return float(self._slice_thickness) if self._slice_thickness else None
+    
+    def modality(self) -> str:
+        return self._modality
+    
+    def series_description(self) -> str:
+        return self._series_description
+    
+    def manufacturer(self) -> str:
+        return self._manufacturer
--- rbeesoftapps/src/rbeesoftapps/pyside6/core/data/dicomseries.py
+++ rbeesoftapps/src/rbeesoftapps/pyside6/core/data/dicomseries.py
@@ -9,12 +9,19 @@ from rbeesoftapps.pyside6.core.data.dicomfile import DicomFile
 class DicomSeries(FileSet):
     def __init__(self, path: str) -> None:
         super(DicomSeries, self).__init__(path)
+        self._series_instance_uid = None
+        self._patient_id = None
+        self._nr_slices = None
+        self._slice_thickness = None
+        self._rows = None
+        self._columns = None
+        self._modality = None
+        self._series_description = None
+        self._manufacturer = None
 
     def load(self) -> bool:
-        self.files().clear()
         if not os.path.isdir(self.path()):
             return False
-        series_instance_uid = None
         for f in os.listdir(self.path()):
             f_path = os.path.join(self.path(), f)
             if f.startswith('.') or not os.path.isfile(f_path):
@@ -22,9 +29,38 @@ class DicomSeries(FileSet):
             file = DicomFile(f_path)
             if not file.load():
                 continue
-            suid = file.data().SeriesInstanceUID
-            if series_instance_uid is None: series_instance_uid = suid
-            if series_instance_uid != suid:
+            suid = file.series_description()
+            if self._series_instance_uid is None: self._series_instance_uid = suid
+            if self._series_instance_uid != suid:
                 raise ValueError('Mismatching series instance UIDs')
+            patient_id = file.patient_id()
+            if self._patient_id is None: self._patient_id = patient_id
+            if self._patient_id != patient_id:
+                raise ValueError('Mismatching patient IDs')
+            slice_thickness = file.slice_thickness()
+            if self._slice_thickness is None: self._slice_thickness = slice_thickness
+            if self._slice_thickness != slice_thickness:
+                raise ValueError('Mismatching slice thicknesses')
+            rows = file.rows()
+            if self._rows is None: self._rows = rows
+            if self._rows != rows:
+                raise ValueError('Mismatching rows')
+            columns = file.columns()
+            if self._columns is None: self._columns = columns
+            if self._columns != columns:
+                raise ValueError('Mismatching columns')
+            modality = file.modality()
+            if self._modality is None: self._modality = modality
+            if self._modality != modality:
+                raise ValueError('Mismatching modalities')
+            series_description = file.series_description()
+            if self._series_description is None: self._series_description = series_description
+            if self._series_description != series_description:
+                raise ValueError('Mismatching series descriptions')
+            manufacturer = file.manufacturer()
+            if self._manufacturer is None: self._manufacturer = manufacturer
+            if self._manufacturer != manufacturer:
+                raise ValueError('Mismatching scanner manufacturers')
             self.files().append(file)
-        return len(self.files()) > 0
+        self._nr_slices = len(self.files())
+        return self._nr_slices > 0
